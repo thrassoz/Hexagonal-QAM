5d4af0001c3513190753151649ccb138b5db7262
Thrassos Oikonomou <thrassosoikonomou@iMac-Thrassos.lan>
2021-05-14 16:24:07 +0300

    update simulations

--- sep_simulation.py
+++ sep_simulation.py
@@ -8,7 +8,13 @@ from scipy.stats import gamma
 
 def proposal(dmin, No):
     R = dmin/2
-    p = 1 - gamma.cdf(R**2,1,0,No) #cdf(x, a, loc=0, scale=1)
+    sigma = math.sqrt(No/2)
+    p = (244/256)*(1 - gamma.cdf(R**2,1,0,No)) + (12/256)*(1 - (gamma.cdf(R**2,1,0,No)/2 + (1/2)*(1-2*(0.5 - math.erf(dmin/(2*sigma)/math.sqrt(2))/2))))#cdf(x, a, loc=0, scale=1)
+    return p
+
+def proposal_first(dmin, No):
+    R = dmin/2
+    p = 1 - gamma.cdf(R**2,1,0,No)
     return p
 
 def existing(dmin, No, K, Kc): # K, Kc are unique numbers for every M-Hqam
@@ -23,19 +29,22 @@ noise = np.arange(0.1, 1, 0.02)
 y1 = np.array([])
 y2 = np.array([])
 y3 = np.array([])
+y4 = np.array([])
 for i in noise:
     temp = np.array([proposal(2, i)])
-    temp2 = np.array([existing(2, i, 33/8, 27/8)])
-    temp3 = np.array([existing2(2, i, 33/8)])
+    temp4 = np.array([proposal_first(2, i)])
+    temp2 = np.array([existing(2, i, 711/128, 171/32)]) #[18]
+    #temp3 = np.array([existing2(2, i, 33/8)]) #[19]
     y1 = np.concatenate((y1,temp))
     y2 = np.concatenate((y2,temp2))
-    y3 = np.concatenate((y3,temp3))
+    #y3 = np.concatenate((y3,temp3))
+    y4 = np.concatenate((y4,temp4))
 # constellation = IrrHQAM(16, 2)  # Generate Constellation with nn_dist = 2
 # sim = np.array([])
 # for i in noise:
 #     idx =  0
 #     errors = 0
-#     while idx < 10000000:
+#     while idx < 1000000:
 #         n = np.random.normal(0,i,1) + 1j*np.random.normal(0,i,1)  # AWGN np.random.normal(mean, sigma, size)
 #         #by changing sigma we change noise power and because energy per symol is fixed due to standard nn_dist we can 
 #         #change SNR parameter
@@ -58,13 +67,14 @@ for i in noise:
 
 # plt.plot(noise, sim, '-')
 # plt.gca().set_yscale('log')
+noise  = 10*np.log10( (141.01/8) / noise) #Eb/No
 plt.plot(noise, y1, 's')
 plt.gca().set_yscale('log')
 plt.plot(noise, y2, 'h')
 plt.gca().set_yscale('log')
-plt.plot(noise, y3, '-')
+plt.plot(noise, y4, '--')
 plt.gca().set_yscale('log')
-plt.legend(["proposed", "existing", "existing2"])
-plt.ylabel('SEP of 16-HQAM')
-plt.xlabel("No")
+plt.legend(["proposed_fix", "existing", "proposed_first"])
+plt.ylabel('SEP of irregular 256-TQAM')
+plt.xlabel("Eb/No")
 plt.show()
